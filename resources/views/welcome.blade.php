2a6ecd4cafd489dcbfa0064befbaca9e1f9f5340
takuya.kusunoki <takuya.kusunoki@softfront-japan.co.jp>
2018-05-22 02:47:06 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,15 @@
-@extends("layouts.app")
+@extends('layouts.app')
 
-@section("content")
-    <div class="center jumbotroon">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
-            {!! link_to_route("sign up now!", null, ["class" => "btn btn-lg btn-primary"]) !!}
+@section('content')
+    @if ("content")
+        <?php $user = Auth::user(); ?>
+        {{ $user->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
